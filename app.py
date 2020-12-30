8115a337f5c30a799b47e851d9f8f3feaee5643f
Tattered75 <YMoyo@cabotfinancial.ie>
2020-12-30 19:34:44 +0000

    updates

--- app.py
+++ app.py
@@ -10,11 +10,9 @@ if os.path.exists("env.py"):
 
 app = Flask(__name__)
 
-
 app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
 app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 app.secret_key = os.environ.get("SECRET_KEY")
-
 
 mongo = PyMongo(app)
 
@@ -23,10 +21,10 @@ mongo = PyMongo(app)
 @app.route("/get_tasks")
 def get_tasks():
     tasks = mongo.db.tasks.find()
-    return render_template("task.html", tasks=tasks)
+    return render_template("tasks.html", tasks=tasks)
 
 
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
-    port=int(os.environ.get("PORT")),
-    debug=True)
+            port=int(os.environ.get("PORT")),
+            debug=True)
