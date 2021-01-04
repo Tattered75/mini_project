02bcda913f7616b904f22ce3c51e39b59cb0cb85
Tattered75 <YMoyo@cabotfinancial.ie>
2021-01-04 17:25:25 +0000

    amended routes add and edit tasks

--- app.py
+++ app.py
@@ -108,8 +108,8 @@ def logout():
     return redirect(url_for("login"))
 
 
-@app.route("/add_task", methods=["GET", "POST"])
-def add_task():
+@app.route("/add_tasks", methods=["GET", "POST"])
+def add_tasks():
     if request.method == "POST":
         is_urgent = "on" if request.form.get("is_urgent") else "off"
         task = {
@@ -125,11 +125,11 @@ def add_task():
         return redirect(url_for("get_tasks"))
 
     categories = mongo.db.categories.find().sort("category_name", 1)
-    return render_template("add_task.html", categories=categories)
+    return render_template("add_tasks.html", categories=categories)
 
 
 @app.route("/edit_task/<task_id>", methods=["GET", "POST"])
-def edit_task(task_id):
+def edit_tasks(task_id):
     if request.method == "POST":
         is_urgent = "on" if request.form.get("is_urgent") else "off"
         submit = {
@@ -145,7 +145,7 @@ def edit_task(task_id):
 
     task = mongo.db.tasks.find_one({"_id": ObjectId(task_id)})
     categories = mongo.db.categories.find().sort("category_name", 1)
-    return render_template("edit_task.html", task=task, categories=categories)
+    return render_template("edit_tasks.html", task=task, categories=categories)
 
 
 @app.route("/delete_task/<task_id>")
